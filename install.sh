037bbdd16200ad0003af5b852e21deb40ff31c34
Michael Jones <michael@autorefactorer.com>
2017-09-09 15:14:23 -0500

    Fixed install script to run every step

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 SETUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Install XCode Tools
-#"${SETUP_DIR}/xcode_install.sh"
+"${SETUP_DIR}/xcode_install.sh"
 
 # Install macports & ports
-#"${SETUP_DIR}/macports_setup.sh"
+"${SETUP_DIR}/macports_setup.sh"
 
 # Install homebrew & brews
-#"${SETUP_DIR}/homebrew_setup.sh"
+"${SETUP_DIR}/homebrew_setup.sh"
 
 # Install dotfiles
 "${SETUP_DIR}/dotfiles/install.sh"
